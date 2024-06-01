19f22a977080bb3d199f7fdddd53c5316807a080
JenManhice <jenmanhice@gmail.com>
2024-06-01 11:38:17 +0200

    improve navbar

--- src/components/Layout/Navbar/Navbar.js
+++ src/components/Layout/Navbar/Navbar.js
@@ -1,42 +1,55 @@
-import React from 'react'
-import illustrate from '../../Assets/vodacom-logo.png'
-import './Navbar.css'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import illustrate from "../../Assets/vodacom-logo.png";
+import "./Navbar.css";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
-    const navigate = useNavigate();
-    const signingOut = () => {
-        localStorage.clear();
-        navigate("/login");
-    }
+  const navigate = useNavigate();
 
-const qrScan = () => {
+  const signingOut = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
+  const qrScan = () => {
     navigate("/qrcode");
-}
-const logoClick = () =>{
+  };
+
+  const logoClick = () => {
     navigate("/");
-}
-    return (
-        <div className='Navbar'>
-            <div className='logo' onClick={logoClick}>
-                <div className='logo-image'>
-                    <img className="image" src={illustrate} alt="logo" />
-                </div>
-                <div className='logo-head'>
-                    <p>Vodacom</p>
-                </div>
+  };
 
-            </div>
-            <div className='Nav-menu'>
-                <div className='sign-out' style={{order: "2"}} onClick={signingOut}>
-                    <i class="fa fa-sign-out" aria-hidden="true" style={{ color: "white", fontSize: "2.2em", cursor: "pointer" }}></i>
-                </div>
-                <div className='qr-code' style={{order: "1"}} onClick={qrScan}>
-                     <i class="fa fa-qrcode" aria-hidden="true" style={{ color: "white", fontSize: "2.2em", cursor: "pointer"}}></i>
-                </div>
-                {/* <LogoutIcon style={{color:"white"}} fontSize="large"/> */}
-            </div>
+  return (
+    <div className="Navbar">
+      <div className="logo" onClick={logoClick}>
+        <div className="logo-image">
+          <img className="image" src={illustrate} alt="logo" />
         </div>
-    )
-}
+        
+        <div className="logo-head">
+          <p>Vodacom</p>
+        </div>
+      </div>
 
-export default Navbar
+      <div className="Nav-menu">
+        <div className="sign-out" style={{ order: "2" }} onClick={signingOut}>
+          <i
+            class="fa fa-sign-out"
+            aria-hidden="true"
+            style={{ color: "#192839", fontSize: "2.2em", cursor: "pointer" }}
+          ></i>
+        </div>
+
+        <div className="qr-code" style={{ order: "1" }} onClick={qrScan}>
+          <i
+            class="fa fa-qrcode"
+            aria-hidden="true"
+            style={{ color: "#192839", fontSize: "2.2em", cursor: "pointer" }}
+          ></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
